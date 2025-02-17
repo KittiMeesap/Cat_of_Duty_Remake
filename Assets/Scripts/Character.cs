f682dcc12818abdeda0f60747a739bd7524b7ee4
kitti <tkt.ms@hotmail.com>
2025-02-17 23:40:14 +0700

    Control Prototype

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Character Stats")]
+    public float maxHealth = 100f;
+    public float currentHealth;
+    public float moveSpeed = 5f;
+    public float armor = 0f;
+
+    protected virtual void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        float finalDamage = Mathf.Max(damage - armor, 1);
+        currentHealth -= finalDamage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        Debug.Log(gameObject.name + " has died.");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CharacterController : Character
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement.normalized * moveSpeed;
+    }
+}
